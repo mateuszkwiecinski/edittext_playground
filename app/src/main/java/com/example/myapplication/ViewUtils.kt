b4b75f5160e2ef6221db51968b9f1e485a2175c4
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2020-10-08 12:41:40 +0200

    wip

--- app/src/main/java/com/example/myapplication/ViewUtils.kt
+++ app/src/main/java/com/example/myapplication/ViewUtils.kt
@@ -2,15 +2,10 @@ package com.example.myapplication
 
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
 import androidx.core.content.getSystemService
 
 fun View.requestFocusWithKeyboard() {
     requestFocus()
     context.getSystemService<InputMethodManager>()
         ?.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
-}
-
-fun selectOnFocus(source: View, target: View) {
-
 }
